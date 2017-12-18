59dc8a6f9d50f6fbc8448f9fabe3db0f63d8ca39
Navpreet <navpreet.ahuja@hotmail.com>
2017-12-19 02:07:58 +0530

    form validation added using html and css

--- src/app/auth/signup.component.ts
+++ src/app/auth/signup.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
 
+import { User } from "./user.model";
+
 @Component({
   selector: 'my-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+	model: User;
+	cpassword = '45';
   constructor() {}
 
   ngOnInit() {
